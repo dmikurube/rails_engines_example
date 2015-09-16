4ffb3fcd19cac7562424caef31d3a1c27e0ec9e3
Dai MIKURUBE <dmikurube@acm.org>
2015-09-16 19:51:12 +0100

    Route from the parent to the child.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount Child::Engine, at: "/child"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
